ed19f22f900d0aafecd5ce5c296750897e5d8c72
youssefemad7 <youssefabdelnaby12123@gmail.com>
2024-10-16 19:23:12 +0300

    the url of site

--- src/js/controller.js
+++ src/js/controller.js
@@ -9,6 +9,7 @@ import addRecipeView from './views/addRecipeView.js';
 import addRecipeView from './views/addRecipeView.js';
 import { async } from 'regenerator-runtime';
 
+//https://forkifyresturant.netlify.app/ the url of the site
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
